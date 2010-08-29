584453edc78e92658f310f25ec2a4d93321b3513
Mathijs de Bruin <drbob@dokterbob.net>
2010-08-29 18:07:58 +0200

    Surlex is tight shit.

---
+++ simplesite/urls.py
@@ -0,0 +1,9 @@
+from surlex.dj import surl
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('simplesite.views',
+    surl(r'^<menu_slug:s>/<submenu_slug:s>/$', 'page', name='submenu'),
+    surl(r'^<menu_slug:s>/$', 'page', name='menu'),
+)
+
