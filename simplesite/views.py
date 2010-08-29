3dbd00b9f96197decd142c1900557bf2f5b29102
Mathijs de Bruin <drbob@dokterbob.net>
2010-08-29 18:09:26 +0200

    Todo

--- simplesite/views.py
+++ simplesite/views.py
@@ -12,6 +12,11 @@ def page(request, menu_slug, submenu_slug=None):
     """
     
     # TODO: FINISH THIS
+    # 1) Assert the menu context_processor is present
+    # 2) Fetch the current menu and submenu from the context
+    # 3) See whether there's a corresponding page
+    # 4)a If so, continue.
+    # 4)b If not: raise 404 or some other error (design decision). 
     context = RequestContext(request)
     return render_to_response('simplesite/page.html', request)
 
