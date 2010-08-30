027660d68d1908255dec6def779fdb31c0a18a21
Mathijs de Bruin <drbob@dokterbob.net>
2010-08-30 15:38:17 +0200

    Debug logging in menu contex_processor and removed redundant import

--- simplesite/context_processors.py
+++ simplesite/context_processors.py
@@ -1,8 +1,9 @@
-from django.core.urlresolvers import resolve
+import logging
+
+from django.core.urlresolvers import resolve, Resolver404
 
 from simplesite.models import Menu, Submenu
 
-from simplesite.urls import urlpatterns
 
 def menu(request):
     """ This function puts into the RequestContext:
@@ -14,16 +15,20 @@ def menu(request):
     
     # Only select visible menu items
     menu_visible = Menu.objects.filter(visible=True)
+    logging.debug('Adding menu items to context: %s', menu_visible)
 
     menu_dict = {'menu_list': menu_visible }
                  
     try:    
         # Use the urlspace configuration from urls.py to
         # resolve menu_slug and submenu_slug parameters
-        view, args, kwargs = resolve(path, urlconf=urlpatterns)
+        # FIXME
+        view, args, kwargs = resolve(request.path, urlconf='simplesite.urls')
+        logging.debug('menu url matched: args=%s, kwargs=%s', args, kwargs)
 
         menu_slug = getattr(kwargs, 'menu_slug', None)
         if menu_slug:
+            logging.debug('menu_slug=%s', menu_slug)
             menu_obj = menu_visible.get(slug=menu_slug)
             
             # Find the corresponding submenu items
@@ -35,6 +40,7 @@ def menu(request):
     
             submenu_slug = getattr(kwargs, 'submenu_slug', None)
             if submenu_slug:
+                logging.debug('submenu_slug=%s', submenu_slug)
                 submenu_obj = submenu_visible.get(slug=submenu_slug)
                 
                 menu_dict.update({'submenu_current': submenu_obj})
@@ -43,6 +49,7 @@ def menu(request):
         # Resolver404: the URL pattern doesn't match any URL
         # Menu.DoesNotExist: the menu_slug found in the URL match any menu
         # Submenu.DoesNotExist: the submenu_slug found doesn't match any submenu
-        pass
+        
+        logging.debug('Current menu item not identified')
     
     return menu_dict
