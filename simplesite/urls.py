1b57ecd2fce3c7bfc53b34cfd041175f518a6006
Mathijs de Bruin <drbob@dokterbob.net>
2010-08-30 15:49:41 +0200

    Whoopsie in urls

--- simplesite/urls.py
+++ simplesite/urls.py
@@ -3,7 +3,7 @@ from surlex.dj import surl
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('simplesite.views',
-    surl(r'^<menu_slug:s>/<submenu_slug:s>/$', 'page', name='submenu'),
-    surl(r'^<menu_slug:s>/$', 'page', name='menu'),
+    surl(r'<menu_slug:s>/<submenu_slug:s>/$', 'page', name='submenu'),
+    surl(r'<menu_slug:s>/$', 'page', name='menu'),
 )
 
