174ee42a6f52e801e7322e1d5e7be9fb8b644dba
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-06 15:49:17 +0200

    Make sure we only show published pages.

--- simplesite/views.py
+++ simplesite/views.py
@@ -8,20 +8,17 @@ from django.template import RequestContext
 from simplesite.models import Page, Menu, Submenu
 
 
-def page(request, menu_slug, submenu_slug=None):
+def page(request, menu_slug=None, submenu_slug=None):
     """ This is, basically, a wrapper around the 
         menu RequestContextProcessor, allowing us
         to capture URL's that have not been captured
         elsewhere in the URL space. 
     """
     
-    logging.debug('No other view found, resorting to page view with \
-                   menu_slug=%s and submenu_slug=%s.' % (menu_slug, submenu_slug))
-
     context = RequestContext(request)
-    
-    menu = context.get('menu_current')
-    if not menu or not menu.page:
+
+    page = context.get('page_current')
+    if not (page and page.publish):
         raise Http404
     
     return render_to_response('simplesite/page.html', context)
