89331cab88bc451c19a0bdd8141fa6331a03aa4f
Mathijs de Bruin <drbob@dokterbob.net>
2010-09-20 15:14:47 +0200

    Added a menu and a submenu sitemap.

--- simplesite/sitemaps.py
+++ simplesite/sitemaps.py
@@ -1,5 +1,14 @@
 from django.contrib.sitemaps import Sitemap
-from simplesite.models import Menu
+from simplesite.models import Menu, Submenu
+
+class MenuSitemap(Sitemap):
+    def items(self):
+        return Menu.objects.filter(visible=True)
+
+
+class SubmenuSitemap(Sitemap):
+    def items(self):
+        return Submenu.objects.filter(visible=True)
 
 
 class PageSitemap(Sitemap):
