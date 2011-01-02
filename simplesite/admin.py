ed768ab8e387ad2c1f4151106147da01c1462c63
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-01-02 22:13:35 +0100

    More refactorization

--- simplesite/admin.py
+++ simplesite/admin.py
@@ -1,5 +1,9 @@
 import logging
 
+logger = logging.getLogger('simplesite')
+
+from sorl.thumbnail.admin import AdminInlineImageMixin
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
@@ -9,75 +13,16 @@ from django.contrib import admin
 from django.contrib.sitemaps import ping_google
 from django.conf.urls.defaults import patterns, url
 
-from tinymce.widgets import TinyMCE
-from tinymce.views import render_to_image_list
-
-from sorl.thumbnail.admin import AdminInlineImageMixin
-
 from models import Menu, Submenu, Page, PageImage, MenuTranslation, \
                    SubmenuTranslation, PageTranslation
-from forms import MenuAdminForm
-from utils import ExtendibleModelAdminMixin
+from baseadmin import BasePageAdmin, BaseMenuAdmin, TinyMCEAdminMixin
 
 from multilingual_model.admin import TranslationInline
 
-logger = logging.getLogger('simplesite')
 
+class PageTranslationInline(TinyMCEAdminMixin, TranslationInline):
+    model = PageTranslation
 
-class BasePageAdmin(admin.ModelAdmin, ExtendibleModelAdminMixin):
-    def get_image_list(self, request, object_id):
-        """ Get a list of available images for this page for TinyMCE to
-            refer to.
-        """
-        object = self._getobj(request, object_id)
-        
-        images = object.pageimage_set.all()
-        image_list = [(unicode(obj), obj.image.url) for obj in images]
-        
-        return render_to_image_list(image_list)
-     
-    
-    def get_urls(self):
-        urls = super(BasePageAdmin, self).get_urls()
-        
-        my_urls = patterns('',
-            url(r'^(.+)/image_list.js$', 
-                self._wrap(self.get_image_list), 
-                name=self._view_name('image_list')),
-        )
-
-        return my_urls + urls
-
-    
-    def save_model(self, request, obj, form, change):
-        super(BasePageAdmin, self).save_model(request, obj, form, change)
-        
-        if not settings.DEBUG and obj.publish:
-            try:
-                ping_google()
-            except Exception:
-                # Bare 'except' because we could get a variety
-                # of HTTP-related exceptions.
-                logger.warning('Error pinging Google while saving %s.' \
-                                    % obj)
-        else:
-            logger.debug('Not pinging Google while saving %s, DEBUG=True.' \
-                            % obj)
-
-
-class TinyMCEAdminMixin(object):
-    @staticmethod
-    def get_tinymce_widget(obj=None):
-        """ Return the appropriate TinyMCE widget. """
-        if obj:
-            image_list_url = reverse('admin:simplesite_page_image_list',\
-                                     args=(obj.pk, ))
-
-            return \
-               TinyMCE(mce_attrs={'external_image_list_url': image_list_url})
-        else:
-            return TinyMCE()
-    
     def get_formset(self, request, obj=None, **kwargs):
         """ Override the form widget for the content field with a TinyMCE
             field which uses a dynamically assigned image list. """
@@ -88,10 +33,6 @@ class TinyMCEAdminMixin(object):
         formset.form.base_fields['content'].widget = self.get_tinymce_widget(obj)
 
         return formset
-
-
-class PageTranslationInline(TinyMCEAdminMixin, TranslationInline):
-    model = PageTranslation
 
 
 class PageImageInline(AdminInlineImageMixin, admin.TabularInline):
@@ -111,20 +52,6 @@ class SubmenuTranslationInline(TranslationInline):
     model = SubmenuTranslation
 
 
-class BaseMenuAdmin(admin.ModelAdmin):
-    #prepopulated_fields = {'slug': ('title',)}
-    list_display_links = ('slug',)
-
-    def admin_page(self, obj):
-        if obj.page:
-            return '<a href="../page/%d/">%s</a>' % (obj.page.id, obj.page)
-        else:
-            return ''
-    admin_page.short_description = Page._meta.verbose_name
-    admin_page.allow_tags = True
-    
-    form = MenuAdminForm
-
 class MenuAdmin(BaseMenuAdmin):
     list_display = ('ordering', 'slug', 'visible', 'admin_page', 'admin_submenu')
     list_filter = ('visible', )
@@ -139,7 +66,8 @@ class MenuAdmin(BaseMenuAdmin):
     admin_submenu.allow_tags = True
 
     inlines = [MenuTranslationInline, ]
-    
+
+
 class SubmenuAdmin(BaseMenuAdmin):
     list_display = ('ordering', 'slug', 'visible', 'admin_page', 'admin_menu')
     list_filter = ('visible', 'menu', )
@@ -150,6 +78,7 @@ class SubmenuAdmin(BaseMenuAdmin):
     admin_menu.allow_tags = True
 
     inlines = [SubmenuTranslationInline, ]
+
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Menu, MenuAdmin)
