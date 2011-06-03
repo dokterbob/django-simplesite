0f7ab19f0a355b67db0048ad037910e7c95c99e2
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-06-03 16:17:04 +0200

    Fixed URL validation

--- simplesite/forms.py
+++ simplesite/forms.py
@@ -4,21 +4,23 @@ from django import forms
 
 from django.utils.translation import ugettext_lazy as _
 
-from django.core.urlresolvers import resolve, Resolver404
-
 logger = logging.getLogger('simplesite')
+
+from django.core.urlresolvers import resolve, Resolver404
 
 
 class MenuAdminForm(forms.ModelForm):
-    def clean_page(self):
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
         # When form fields don't validate, they won't end up in
         # cleaned_data, hence we should account for them not to
         # be there.
-        page = self.cleaned_data.get('page', None)
-        slug = self.cleaned_data.get('slug', None)
+        page = cleaned_data.get('page', None)
+        slug = cleaned_data.get('slug', None)
 
         if not page and slug:
-            menu = self.cleaned_data.get('menu', None)
+            menu = cleaned_data.get('menu', None)
             if menu:
                 path = '/%s/' % menu.slug
             else:
@@ -26,17 +28,20 @@ class MenuAdminForm(forms.ModelForm):
 
             path += '%s/' % slug
 
-            logger.debug('Checking whether the path %s yields a view in \
-                          the URL space.', path)
+            logger.debug('Checking whether the path %s yields a view.',
+                         path)
 
             # This should always work.
             try:
                 view, args, kwargs = resolve(path)
             except Resolver404:
-                raise forms.ValidationError(_('Without a page, this menu \
-                        item would point nowhere. Please select a page for it \
-                        to link to.'))
+                error_message = \
+                    _('Without a page, this menu \
+                       item would point nowhere. Please select a page for it \
+                       to link to.')
+                self._errors['page'] = self.error_class([error_message])
 
-        # Be sure to return the original value
-        return page
+                # Remove invalid data
+                del cleaned_data['page']
 
+        return cleaned_data
