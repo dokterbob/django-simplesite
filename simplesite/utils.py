0116378a4567e46e494a9a46692aaf39d8166b58
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-01-24 22:02:37 +0100

    ExtendibleModelAdmin from django-newsletter.

--- simplesite/utils.py
+++ simplesite/utils.py
@@ -1,24 +1,33 @@
 """ http://djangosnippets.org/snippets/1804/ """
 
-from django.contrib.admin.util import unquote
 from functools import update_wrapper
+
+from django.contrib.admin.util import unquote
+from django.utils.encoding import force_text
 
 
 class ExtendibleModelAdminMixin(object):
     def _getobj(self, request, object_id):
             opts = self.model._meta
-            app_label = opts.app_label
 
             try:
-                obj = self.queryset(request).get(pk=unquote(object_id))
+                obj = self.get_queryset(request).get(pk=unquote(object_id))
             except self.model.DoesNotExist:
                 # Don't raise Http404 just yet, because we haven't checked
-                # permissions yet. We don't want an unauthenticated user to be able
-                # to determine whether a given object exists.
+                # permissions yet. We don't want an unauthenticated user to
+                # be able to determine whether a given object exists.
                 obj = None
 
             if obj is None:
-                raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
+                raise Http404(
+                    _(
+                        '%(name)s object with primary key '
+                        '%(key)r does not exist.'
+                    ) % {
+                        'name': force_text(opts.verbose_name),
+                        'key': force_text(object_id)
+                    }
+                )
 
             return obj
 
@@ -28,7 +37,7 @@ class ExtendibleModelAdminMixin(object):
         return update_wrapper(wrapper, view)
 
     def _view_name(self, name):
-        info = self.model._meta.app_label, self.model._meta.module_name, name
+        info = self.model._meta.app_label, self.model._meta.model_name, name
 
         return '%s_%s_%s' % info
 
