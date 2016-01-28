b90001c955711d50136b126bcf75948d53bff760
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-01-28 15:32:34 +0100

    Minor de-uglification.

--- simplesite/forms.py
+++ simplesite/forms.py
@@ -14,18 +14,18 @@ logger = logging.getLogger('simplesite')
 class MenuAdminForm(forms.ModelForm):
     def clean(self):
         page = self.cleaned_data['page']
-        
+
         if not page:
             slug = self.cleaned_data['slug']
-            
-            if 'menu' in self.cleaned_data:                
+
+            if 'menu' in self.cleaned_data:
                 menu = self.cleaned_data['menu']
                 path = '/%s/' % menu.slug
             else:
                 path = '/'
-            
+
             path += '%s/' % slug
-            
+
             logger.debug('Checking whether the path %s yields a view in the URL space.' % path)
 
             # This should always work.
@@ -35,7 +35,5 @@ class MenuAdminForm(forms.ModelForm):
                 raise forms.ValidationError(_('Without a page, this menu \
                         item would point nowhere. Please select a page for it \
                         to link to.'))
-        
-        return super(MenuAdminForm, self).clean()
-        
 
+        return super(MenuAdminForm, self).clean()
